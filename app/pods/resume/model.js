6319b11a81ecd82e3880867f72748e60763cf182
Christian Kraft <csk52@drexel.edu>
2019-03-07 21:51:15 -0500

    resume creation

--- app/pods/resume/model.js
+++ app/pods/resume/model.js
@@ -4,7 +4,7 @@ export default DS.Model.extend({
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
   description: DS.attr('string'),
-  experiences: DS.hasMany('resume'),
-  skills: DS.hasMany('skill'),
+  experiences: DS.hasMany('experience', { inverse: 'resume' }),
+  skills: DS.hasMany('skill', { inverse: 'resume' }),
   user: DS.belongsTo('user', { inverse: 'resumes' })
 });
