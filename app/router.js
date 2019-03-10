68144f84306ec73a224959747cb5320b73188480
Christian Kraft <csk52@drexel.edu>
2019-03-09 20:52:32 -0500

    added resume home route

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ const Router = EmberRouter.extend({
 Router.map(function() {
   this.route('dashboard', { path: '/' });
   this.route('resume', { path: '/resume/:resume_id' }, function() {
+    this.route('home', { path: "/" });
     this.route('experiences', { path: '/experiences' } );
     this.route('skills', { path: '/skills' });
     this.route('contact', { path: '/contact' });
