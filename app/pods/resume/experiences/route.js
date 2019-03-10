9243a1b0a0db5ab2cac5dadabd95aa5b3f8489b4
Christian Kraft <csk52@drexel.edu>
2019-03-09 20:53:11 -0500

    added components

--- app/pods/resume/experiences/route.js
+++ app/pods/resume/experiences/route.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    let resume = this.modelFor('post');
+    let resume = this.modelFor('resume');
     return resume.get('experiences');
   }
 });
--- app/pods/resume/model.js
+++ app/pods/resume/model.js
@@ -1,8 +1,14 @@
 import DS from 'ember-data';
+import { computed } from '@ember/object';
 
 export default DS.Model.extend({
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
+  fullName: computed('firstName', 'lastName', function() {
+     let firstName = this.get('firstName');
+     let lastName = this.get('lastName');
+     return `${firstName} ${lastName}`;
+  }),
   description: DS.attr('string'),
   experiences: DS.hasMany('experience', { inverse: 'resume' }),
   skills: DS.hasMany('skill', { inverse: 'resume' }),
