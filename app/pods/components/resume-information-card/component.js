b6a3379de23b0600a6ab1c6c75af83da63abccdb
Christian Kraft <csk52@drexel.edu>
2019-03-11 12:55:26 -0400

    added most-recent experience title as resume title

--- app/pods/components/resume-information-card/component.js
+++ app/pods/components/resume-information-card/component.js
@@ -1,7 +1,22 @@
 import Component from '@ember/component';
+import {
+  computed
+} from '@ember/object';
 
 export default Component.extend({
   classNames: ['ui', 'centered', 'card', 'margin: auto;'],
+  currentTitle: computed('resume.experiences.@each.endDate', function() {
+    let output = "";
+    let experiences = this.get('resume.experiences');
+    let sortedExperiences = experiences.sortBy('startDate');
+    sortedExperiences.forEach(function(experience) {
+      let endDate = experience.get('endDate');
+      if (new Date().getMonth() == endDate.getMonth()) {
+        output += experience.get('title') + " ";
+      }
+    });
+    return output;
+  }),
   actions: {
     openModal: function() {
       this.$('.ui.modal').modal('show');
