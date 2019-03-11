5d018e073ebc8d530f4fc62055333b80c70765f6
Christian Kraft <csk52@drexel.edu>
2019-03-11 12:46:04 -0400

    added contact page content

--- app/pods/resume/contact/controller.js
+++ app/pods/resume/contact/controller.js
@@ -1,4 +1,24 @@
-import Controller from '@ember/controller';
+import Controller, {
+  inject as controller
+} from '@ember/controller';
+import {
+  alias
+} from '@ember/object/computed';
+import {
+  computed
+} from '@ember/object';
+import {
+  inject as service
+} from '@ember/service';
+import {
+  equal
+} from '@ember/object/computed';
+import {
+  isEqual
+} from '@ember/utils';
 
 export default Controller.extend({
+  resume: alias('model'),
+  resumeController: controller('resume'),
+  isOwner: alias('resumeController.isOwner')
 });
